bede91860601d3515bc208c5e3110012cb2f99a4
Letícia Souza <letisosan@outlook.com>
2025-08-13 21:38:54 -0300

    aula 13 08

--- main.c
+++ main.c
@@ -1,81 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    double raio;
-    double area;
-    double peri;
-    double pi;
 
-    printf("informe o valor do raio do circulo:\n");
-    scanf("%lf", &raio);
+   double num, quadrado, cubo;
 
-    pi = 3.14159;
-    area = pi * raio * raio;
+   printf("informe um numero:\n");
+   scanf("%lf", &num);
 
-    printf("o valor da area eh:%2.lf\n", area);
+   quadrado = pow (num, 2);
+   cubo = pow (num, 3);
 
-    peri = 2 * pi * raio;
-
-    printf("o valor do perimetro eh: %2.lf\n", peri);
-
-    int idade;
-    int dias;
-    int vividos;
-
-    printf("informe a sua idade:\n");
-    scanf("%d", &idade);
-
-    dias = 365;
-
-    vividos = idade * dias;
-    printf("voce viu essa quantidade de dias:%d\n", vividos);
-
-    int num1;
-    double porc;
-
-    printf("informe um numero:\n");
-    scanf("%d", &num1);
-
-    porc = num1 * 0.04;
-    printf("esse numero mais 4pc eh:%2.lf\n", porc);
+   printf("o valor do cubo eh: %.2lf\n", cubo);
+   printf("o valor do quadrado eh: %.2lf\n", quadrado);
 
 
+   double num1, num2;
+   printf("informe um numero:\n");
+   scanf("%lf", &num1);
+   printf("informe outro numero:\n");
+   scanf("%lf", &num2);
 
-   int nota1;
-   int nota2;
-   int nota3;
+   double aux, raiz;
 
-   printf("informe a primeira nota\n");
-   scanf("%d", &nota1);
-   printf("informe a segunda nota\n");
-   scanf("%d", &nota2);
-   printf("informe a terceira nota\n");
-   scanf("%d", &nota3);
+   aux = num1 * num2;
 
-   nota1 = nota1*0.2;
-   nota2 = nota2*0.3;
-   nota3 = nota3*0.5;
+   raiz = sqrt (aux);
 
-   int media = nota1 + nota2 + nota3;
+   printf("a raiz quadrada da multiplicação desses numeros eh:%.2lf\n", raiz);
 
-   printf("a media final eh: %d \n", media);
+   return 0;
 
-   int dist;
-   int temp;
-
-   printf("informe a distancia percorrida\n");
-   scanf("%d", &dist);
-
-   printf("informe o tempo:\n");
-   scanf("%d", &temp);
-
-   int vm = dist / temp;
-   printf("a velocidade media eh:%d\n", vm);
-
-
-
-
-    return 0;
 }

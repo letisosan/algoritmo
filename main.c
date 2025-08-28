d787b01058a84eb4091bb9be89396c25dbf11274
Letícia Souza <letisosan@outlook.com>
2025-08-27 21:24:25 -0300

    switch case

--- main.c
+++ main.c
@@ -1,49 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
 
-     int n1;
-    int n2;
+    char conceito;
+    printf("informe o conceito do aluno:\n");
+    scanf("%c", &conceito);
 
-    printf("informe um numero:\n");
-    scanf("%d", &n1);
-    printf("informe outro numero:\n");
-    scanf("%d", &n2);
-
-
-    if (n1%2==0 && n2%2==0) {
-        printf("os numeros sao pares");
-    }
-    else if (n1%2==0 || n2%2==0) {
-        printf("um eh par");
-            }
-    else {
-        printf("nao sao pares\n");
-    }
-
-
-    double altura, peso;
-    char sexo;
-
-    printf("informe a sua altura em metros:\n");
-    scanf("%lf", &altura);
-    printf("informe o seu sexo:\n");
-    scanf(" %c", &sexo);
-
-    if(sexo == 'm' || sexo == 'M'){
-        peso = (72.7 * altura) -58;
-        printf("peso ideal: %.2lf\n", peso);
-    }
-    else if(sexo == 'f' || sexo == 'F'){
-        peso = (62.1 * altura) - 44.7;
-        printf("peso ideal: %.2lf\n");
-    }
-    else {
-        printf("opcao invalida!!!!");
+    switch (conceito) {
+        case 'A': {
+        printf("excelente\n");
+        break;
+        }
+        case 'B': {
+        printf("bom\n");
+        break;
+        }
+        case 'C': {
+        printf("regular\n");
+        break;
+        }
+        case 'D': {
+        printf("reprovado");
+        break;
+        }
+        default: {
+        printf("opcao invalida");
+        }
     }
 
     return 0;
 }
-
